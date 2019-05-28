4f7c6071a5b1d04b7a222c525ef225d0f73d5fe9
Bartosz Sosna <brtsos@gmail.com>
2019-05-28 23:12:35 +0200

    Adjust styles

--- demo/app/restart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/ash
-
-cd $(dirname $(realpath $0))
-
-cp -f ./data/.taskrc /root/
-cp -rf ./data/.task /root/
-
-echo $(date +"%Y-%m-%d %H:%M:%S") Taskwarrior database resetted > /var/log/reset.log
--- demo/app/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd $(dirname $(realpath $0))
-
-mkdir -p /root/.task
-
-./restart.sh && task && task-web -p 80 -L -F
